bb8f3e7712a48fa799bd219b3a8d6f7d4dd176b8
Erik Österberg <erik82osterberg@gmail.com>
2019-08-07 21:35:31 +0200

    terser export syntax

--- index.ts
+++ index.ts
@@ -1,20 +1,3 @@
-import { setArr, unsetArr, filter, filterIn, remove, removeIn } from "./src/arrayFunctions"
-import { setObj, unsetObj, merge, mergeIn } from "./src/objectFunctions"
-import { set, unset, setIn, unsetIn } from './src/genericFunctions'
-
-export {
-  set,
-  unset,
-  setIn,
-  unsetIn,
-  mergeIn,
-  setArr,
-  unsetArr,
-  filter,
-  filterIn,
-  setObj,
-  unsetObj,
-  merge,
-  remove,
-  removeIn,
-}
+export { setArr, unsetArr, filter, filterIn, remove, removeIn } from "./src/arrayFunctions"
+export { setObj, unsetObj, merge, mergeIn } from "./src/objectFunctions"
+export { set, unset, setIn, unsetIn } from './src/genericFunctions'
