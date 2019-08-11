f9d08bad524dbb454eb5b983b471595cf3fc039a
Erik Österberg <erik82osterberg@gmail.com>
2019-08-11 13:36:27 +0200

    add type signatures

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-export type AnyArray = any[]
-export type AnyObject = {[key: string]: any} 
-export type AnyState = AnyArray | AnyObject
-export type KeyPath = (string | number)[]
+export type Key = string | number
+export type KeyPath = Key[]
+export type Predicate = <T>(item: T, index: number) => boolean
 
-const normalizeIndex = (arr: AnyArray, index: number) => {
+const normalizeIndex = (arr: any[], index: number) => {
   if (index < 0) {
     if (arr.length === 0) return 0
     
@@ -15,12 +14,12 @@ const normalizeIndex = (arr: AnyArray, index: number) => {
   return index
 }
 
-const dropIndex = (arr: AnyArray, index: number) =>
+const dropIndex = <T>(arr: T[], index: number) =>
   index === -1 ?
     arr :
     arr.slice(0, index).concat(arr.slice(index + 1))
 
-const setArr = (arr: AnyArray, index: number, value: any) => {
+const setArr = (arr: any[], index: number, value) => {
   index = normalizeIndex(arr, index)
 
   if (arr[index] === value) return arr
@@ -30,10 +29,10 @@ const setArr = (arr: AnyArray, index: number, value: any) => {
   return next
 }
 
-const unsetArr = (arr: AnyArray, index: number) =>
+const unsetArr = (arr: any[], index: number) =>
   dropIndex(arr, normalizeIndex(arr, index))
 
-const setObj = (obj, key, value) => {
+const setObj = (obj, key: Key, value) => {
   if (obj[key] === value) return obj
 
   const next = {}
@@ -43,7 +42,7 @@ const setObj = (obj, key, value) => {
   return next
 }
 
-const unsetObj = (obj, key) => {
+const unsetObj = (obj, key: Key) => {
   if (key in obj) {
     const next = {}
     for (var k in obj) {
@@ -57,15 +56,15 @@ const unsetObj = (obj, key) => {
 
 const {isArray} = Array
 
-const set = (obj, key, value) => (
+const set = <T>(obj: T, key: Key, value): T => (
   isArray(obj) ? setArr : setObj
 )(obj, key, value)
 
-const unset = (obj, key) => (
+const unset = <T>(obj: T, key: Key): T => (
   isArray(obj) ? unsetArr : unsetObj
 )(obj, key)
 
-const operationIn = (obj: AnyState, keys: KeyPath, operation, value) => set(
+const operationIn = (obj, keys: KeyPath, operation, value) => set(
   obj,
   keys[0],
   keys.length > 1 ?
@@ -73,37 +72,37 @@ const operationIn = (obj: AnyState, keys: KeyPath, operation, value) => set(
     operation(obj[keys[0]], value)
 )
 
-const setIn = (obj, keys, value) =>
-  operationIn(obj, keys, (obj, value) => value, value)
+const setIn = <T>(obj: T, keys: KeyPath, value): T =>
+  operationIn(obj, keys, (_obj, value) => value, value)
 
-const unsetIn = (obj, keys, keyOrIndex) =>
-  operationIn(obj, keys, unset, keyOrIndex)
+const unsetIn = <T>(obj: T, keys: KeyPath, keyToUnset: Key): T =>
+  operationIn(obj, keys, unset, keyToUnset)
 
-const push = (arr: AnyArray, value: any) =>
+const push = <T>(arr: T[], value: T) =>
   arr.concat(value)
 
-const pushIn = (arr: AnyArray, keys: KeyPath, value) =>
-  operationIn(arr, keys, push, value)
+const pushIn = <T>(obj: T, keys: KeyPath, value): T =>
+  operationIn(obj, keys, push, value)
 
-const unshift = (arr: AnyArray, value: any) =>
+const unshift = <T>(arr: T[], value: T) =>
   [value].concat(arr)
 
-const unshiftIn = (arr: AnyArray, keys: KeyPath, value) =>
-  operationIn(arr, keys, unshift, value)
+const unshiftIn = <T>(obj: T, keys: KeyPath, value): T =>
+  operationIn(obj, keys, unshift, value)
 
-const remove = (arr: AnyArray, value: any) =>
+const remove = <T>(arr: T[], value: T): T[] =>
   dropIndex(arr, arr.indexOf(value))
 
-const removeIn = (obj: AnyState, keys: KeyPath, value) =>
+const removeIn = <T>(obj: T, keys: KeyPath, value): T =>
   operationIn(obj, keys, remove, value)
 
-const removeWhere = (arr: AnyArray, predicate) =>
+const removeWhere = <T>(arr: T[], predicate: Predicate) =>
   dropIndex(arr, arr.findIndex(predicate))
 
-const removeInWhere = (obj: AnyState, keys: KeyPath, predicate) =>
+const removeInWhere = <T>(obj: T, keys: KeyPath, predicate: Predicate): T =>
   operationIn(obj, keys, removeWhere, predicate)
 
-const filter = (arr: AnyArray, predicate) => {
+const filter = <T>(arr: T[], predicate: Predicate) => {
   var differIndex = arr.findIndex((item, index) => !predicate(item, index))
   if (differIndex === -1) { return arr }
 
@@ -116,34 +115,28 @@ const filter = (arr: AnyArray, predicate) => {
   return next
 }
 
-const filterIn = (obj: AnyState, keys: KeyPath, predicate) =>
+const filterIn = <T>(obj: T, keys: KeyPath, predicate: Predicate): T =>
   operationIn(obj, keys, filter, predicate)
 
-const merge = (obj, withObj) => {
-  var differ = false
+const merge = <T>(obj: T, withObj) => {
   for (var k in withObj) {
     if (obj[k] !== withObj[k]) {
-      differ = true
-      break
+      const next = {} as T
+      for (k in obj) { next[k] = obj[k] }
+      for (k in withObj) { next[k] = withObj[k] }
+      return next
     }
-  }
-
-  if (differ) {
-    var next = {}
-    for (var k in obj) { next[k] = obj[k] }
-    for (var k in withObj) { next[k] = withObj[k] }
-    return next
   }
 
   return obj
 }
 
-const mergeIn = (obj, keys, withObj) =>
+const mergeIn = <T>(obj: T, keys: KeyPath, withObj): T =>
   operationIn(obj, keys, merge, withObj)
 
-const without = (obj, keysToDrop: string[]) => {
+const without = <T>(obj: T, keysToDrop: string[]): T => {
   if (keysToDrop.some(key => key in obj)) {
-    const next = {}
+    const next = {} as T
     for (var key in obj) {
       if (keysToDrop.indexOf(key) === -1) {
         next[key] = obj[key]
@@ -155,7 +148,7 @@ const without = (obj, keysToDrop: string[]) => {
   return obj
 }
 
-const withoutIn = (obj, keys, keysToDrop: string[]) =>
+const withoutIn = <T>(obj: T, keys: KeyPath, keysToDrop: string[]): T =>
   operationIn(obj, keys, without, keysToDrop)
 
 export {
